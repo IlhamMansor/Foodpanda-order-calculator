2ea6a6eae4677caeb23407a51ff8ba7351d3523b
IlhamMansor <51931338+IlhamMansor@users.noreply.github.com>
2021-12-19 02:52:36 +0800

    Create calculate.js

---
+++ calculate.js
@@ -0,0 +1,22 @@
+const allOrders = document.querySelectorAll('div[class="order-list-item"]');
+
+let totalspend = 0;
+var noOfOrder = 0;
+
+const toRound = value => Math.round((value + Number.EPSILON) * 100) / 100
+
+for (order of allOrders){
+    let totalspend_temp = parseFloat(order.querySelector('span[class="total"]').innerText.slice(3));
+    let vendor = order.querySelector('span[class="vendor-name"]').innerText;
+    let whatOrder = order.querySelector('div[class="order-product-list"]').innerText;
+    let isCancel = order.lastElementChild.querySelector('div [class="order-status"]');
+    if(isCancel == null){
+    totalspend += totalspend_temp;
+    noOfOrder++;
+    totalspendrounded = toRound(totalspend);
+    console.log(`order ${noOfOrder} : (${vendor}) - ${whatOrder}, MYR${totalspend_temp}`);
+    }
+}
+
+totalspendrounded = toRound(totalspend);
+console.log(`total : MYR${totalspendrounded}`);
